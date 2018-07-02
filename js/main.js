975077212714a242a01f3d931246f5f38ac0bc3d
evoxtorm <evostorm96@gmail.com>
2018-07-02 10:39:37 +0530

    Review change

--- js/main.js
+++ js/main.js
@@ -173,7 +173,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = `An image of ${restaurant.name}`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
